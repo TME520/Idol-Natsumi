18f49f26f452ee1583451e9550827751abb29c0e
TME520 <tme520@gmail.com>
2025-11-09 11:07:37 +1100

    NAP screen (8)

--- idol_natsumi_v0_6_1012/idol_natsumi_v0_6_1012.ino
+++ idol_natsumi_v0_6_1012/idol_natsumi_v0_6_1012.ino
@@ -85,7 +85,7 @@ unsigned long sessionStart = 0;           // millis() when NEW_GAME starts
 unsigned long playtimeTotalMs = 0;        // total playtime in ms (could persist later)
 int lastAgeTick = 0;
 const unsigned long microWait = 60;
-const unsigned long shortWait = 800;
+const unsigned long shortWait = 400;
 const unsigned long mediumWait = 3200;
 const unsigned long longWait = 6400;
 
